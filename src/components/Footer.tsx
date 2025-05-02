a8b5d03499e16c8225a8d92a5bff97d51174520f
“[firstname <ojhaankush808@gmail.com>
2025-05-02 11:40:34 +0530

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer: React.FC = () => (
   <footer className="text-center text-xs text-foreground/50 mt-12 pb-6">
-    &copy; {new Date().getFullYear()} Ankush Kumar Ojha Jee. Crafted ojha machine with React & Tailwind CSS. hello
+    &copy; {new Date().getFullYear()} shubham Kumar Ojha Jee. Crafted ojha machine with React & Tailwind CSS. hello
   </footer>
 );
 
